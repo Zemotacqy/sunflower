138fb0985798e08f94c53aa1ef64c80a9caa8a10
Zemotacqy <mranjan1398@gmail.com>
2024-12-24 11:53:14 +0530

    feat: standard tests with 52 tests

---
+++ app/src/androidTest/java/com/google/samples/apps/sunflower/compose/plantdetail/PlantDetailComposeTestDup.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower.compose.plantdetail
+
+import android.content.ContentResolver
+import android.net.Uri
+import androidx.annotation.RawRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.core.net.toUri
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.samples.apps.sunflower.compose.plantdetail.PlantDetails
+import com.google.samples.apps.sunflower.compose.plantdetail.PlantDetailsCallbacks
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.test.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class PlantDetailComposeTestDup {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun plantDetails_checkIsNotPlanted() {
+        startPlantDetails(isPlanted = false)
+        composeTestRule.onNodeWithText("Apple").assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("Add plant").assertIsDisplayed()
+    }
+
+    @Test
+    fun plantDetails_checkIsPlanted() {
+        startPlantDetails(isPlanted = true)
+        composeTestRule.onNodeWithText("Apple").assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("Add plant").assertDoesNotExist()
+    }
+
+    @Test
+    fun plantDetails_checkGalleryNotShown() {
+        startPlantDetails(isPlanted = true, hasUnsplashKey = false)
+        composeTestRule.onNodeWithContentDescription("Gallery Icon").assertDoesNotExist()
+    }
+
+    @Test
+    fun plantDetails_checkGalleryIsShown() {
+        startPlantDetails(isPlanted = true, hasUnsplashKey = true)
+        composeTestRule.onNodeWithContentDescription("Gallery Icon").assertIsDisplayed()
+    }
+
+    private fun startPlantDetails(isPlanted: Boolean, hasUnsplashKey: Boolean = false) {
+        composeTestRule.setContent {
+            PlantDetails(
+                plant = plantForTesting(),
+                isPlanted = isPlanted,
+                callbacks = PlantDetailsCallbacks({ }, { }, { }, { }),
+                hasValidUnsplashKey = hasUnsplashKey
+            )
+        }
+    }
+}
